9b383fa6452f7d85755336fc7453682b5107aaed
GeraldineG30 <geraldines0230@gmail.com>
2026-03-24 23:50:03 -0500

    welcome.py file add

---
+++ welcome.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+# FOR NAME INPUT AND DECORATION
+name = input("Enter your name: ")
+print ("\n" + "=D  " * 30)
+print (f" Welcome {name}!")
+print (":D  " * 30)
+
+# FOR DATETIME MODULE
+time_now = datetime.now()
+print (f"The Current Date and Time: {time_now}")
+
+# DIFFERENT TYPES OF VARIABLES
+middle_name = "Sara" # str
+age = 23             # int
+height = 5.3         # float
+
+# EXTRA F-STRINGS
+print (f"{middle_name} is {age} years old")
+print (f"{middle_name} height is {height} ft")
+
