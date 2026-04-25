a764e6df06846a5d53110690bca9c5e56d91f766
GeraldineG30 <geraldines0230@gmail.com>
2026-04-24 21:06:01 -0500

    Fish Class

---
+++ week05/fish.py
@@ -0,0 +1,19 @@
+from pet import Pet
+
+class Fish(Pet):
+    def __init__(self, name):
+        super().__init__(name, "Fish")
+
+    def feed(self):
+        self._hunger = max(0, self._hunger - 5)
+        # self._happiness += 5 ... does fish get happier after eating?
+        return f"{self._name} Glub..Glub...Glub"
+    
+    def play(self): # Not sure if a fish can play though...
+        self._happiness = max(100, self._happiness + 1)
+        self._energy = max(0, self._energy - 1)
+        return f"{self._name} is swimming happily"
+    
+    def sleep(self):
+        self._energy = max(0, self._energy + 25)
+        return f"{self._name} is sleeping....let's check if it's not a secret fairy odd parent"
