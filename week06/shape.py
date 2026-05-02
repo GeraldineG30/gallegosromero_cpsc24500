7afcb8f42627f07680aac9ab83949d112d36f392
GeraldineG30 <geraldines0230@gmail.com>
2026-05-02 15:01:24 -0500

    Shape class

---
+++ week06/shape.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+    @abstractmethod
+    def describe(self):
+        pass
