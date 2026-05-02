9f2c62d44c8044d6932fc316356d0d9f84af4a1f
GeraldineG30 <geraldines0230@gmail.com>
2026-05-02 16:19:48 -0500

    Gallery class

---
+++ week06/gallery.py
@@ -0,0 +1,30 @@
+
+class Gallery:
+    def __init__(self, name):
+        self._name = name
+        self._shapes = []
+    
+    def add_shape(self, shape):
+        self._shapes.append(shape)
+
+    def total_area(self):
+        if self._shapes:
+            return sum(shape.area() for shape in self._shapes)
+        else:
+            0.0
+    
+    def largest_shape(self):
+        if not self._shapes:
+            return None
+        largest = self._shapes [0]
+        for s in self._shapes[1:]:
+            if s.area() > largest.area():
+                largest = s
+        return largest
+    
+    def display_all(self):
+        print(f"Gallery: {self._name}")
+        for shape in self._shapes:
+            print(f"    {shape.describe()}")
+            print(f"    Area: {shape.area():.2f}")
+            print(f"    Perimeter: {shape.perimeter():.2f}")
