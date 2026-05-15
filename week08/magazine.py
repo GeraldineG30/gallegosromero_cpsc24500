ef6c3fabe915c449337ec2738d2fa19210ce862e
GeraldineG30 <geraldines0230@gmail.com>
2026-05-14 22:35:43 -0500

    indentation mistake correction

--- week08/magazine.py
+++ week08/magazine.py
@@ -8,16 +8,16 @@ class Magazine(LibraryItem):
         self._issue_number = int(issue_number)
         self._month = month
 
-        @property
-        def issue_number(self):
-            return self._issue_number
+    @property
+    def issue_number(self):
+        return self._issue_number
         
-        @property
-        def month(self):
-            return self._month
+    @property
+    def month(self):
+        return self._month
         
-        def get_item_type(self):
-            return "Magazine"
+    def get_item_type(self):
+        return "Magazine"
         
-        def __str__(self):
-            return(super().__str__(), f" | Issue: {self.issue_number}, Month: {self.month}")
+    def __str__(self):
+        return(super().__str__(), f" | Issue: {self.issue_number}, Month: {self.month}")
